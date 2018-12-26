d82e0051a5dbf7b75d123e94f9bfae9a9c6c11dd
Ryan Lewis <rlewis77@gatech.edu>
2018-12-25 23:23:00 -0800

    update stack

--- Stack.py
+++ Stack.py
@@ -5,13 +5,10 @@ insert_first, delete_first, push, and pop.
 Think about this while you're implementing:
 why is it easier to add an "insert_first"
 function than just use "append"?"""
-
-
 class Element(object):
     def __init__(self, value):
         self.value = value
         self.next = None
-
 
 class LinkedList(object):
     def __init__(self, head=None):
@@ -27,29 +24,27 @@ class LinkedList(object):
             self.head = new_element
 
     def insert_first(self, new_element):
-        "Insert new element as the head of the LinkedList"
         new_element.next = self.head
         self.head = new_element
 
     def delete_first(self):
-        "Delete the first (head) element in the LinkedList as return it"
-        deleted = self.head
         if self.head:
-            self.head = self.head.next
-            deleted.next = None
-        return deleted
-
+            deleted_element = self.head
+            self.head = deleted_element.next
+            # temp = deleted_element.next
+            # self.head = temp
+            return deleted_element
+        else: # if there's nothing in the stack
+            return None
 
 class Stack(object):
-    def __init__(self, top=None):
+    def __init__(self,top=None):
         self.ll = LinkedList(top)
 
     def push(self, new_element):
-        "Push (add) a new element onto the top of the stack"
         self.ll.insert_first(new_element)
 
     def pop(self):
-        "Pop (remove) the first element off the top of the stack and return it"
         return self.ll.delete_first()
 
 
