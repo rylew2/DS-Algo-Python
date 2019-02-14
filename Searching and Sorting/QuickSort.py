c87d62a1faace54ebd0d91735dee853861fb1c34
Ryan Lewis <rlewis77@gatech.edu>
2019-02-14 14:57:22 -0800

    bucketsort

--- Searching and Sorting/QuickSort.py
+++ Searching and Sorting/QuickSort.py
@@ -35,18 +35,17 @@ def partition(arr,low,high):
 def quickSort(arr,low,high):
     if low < high:
 
-        # pi is partitioning index, arr[p] is now
-        # at right place
+        # pi is partitioning index, arr[p] is now at right place
         pi = partition(arr,low,high)
 
-        # Separately sort elements before 
-        # partition and after partition 
+        # Separately sort elements before partition and after partition
         quickSort(arr, low, pi-1)
         quickSort(arr, pi+1, high)
 
 
 # Driver code to test above
-arr = [10, 7, 8, 9, 1, 5]
+arr = [4,3,2,1]
+# arr = [10, 7, 8, 9, 1, 5]
 n = len(arr)
 quickSort(arr,0,n-1)
 print(arr)
