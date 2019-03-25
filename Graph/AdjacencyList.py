9fcc6e481ef8238699120c8d2d1b48e3c0aaff12
rylew2 <>
2019-03-25 15:08:44 -0700

    adjlist

---
+++ Graph/AdjacencyList.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(set)
+
+    # O(1)
+    def addEdge(self, u, v):
+        self.graph[u].add(v)
+
+    # O(1)
+    def removeEdge(self, u, v):
+        self.graph[u].remove(v)
+
+    # O(1)
+    def addVertex(self, v):
+        self.graph[v]
+
+    # O(V)
+    def removeVertex(self, v):
+        del self.graph[v]
+        for vert in self.graph:
+            if v in self.graph[vert]:
+                self.graph[vert].remove(v)
+    # O(1)
+    def queryEdge(self, u, v ):
+        return v in self.graph[u]
+
+    def printGraph(self):
+        for v in self.graph:
+            print('{} : {}'.format(v, self.graph[v]))
+
+
+
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+g.printGraph()
+
+
