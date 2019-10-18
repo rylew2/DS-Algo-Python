8530a49981a8d4e731cc59d11948a9d69921be1f
Ryan Lewis <rlewis77@gatech.edu>
2019-10-17 22:55:11 -0700

    graph updates

--- Graph/DFS/DFSDetectcycle.py
+++ Graph/DFS/DFSDetectcycle.py
@@ -10,26 +10,50 @@ class Graph:
     def addVertex(self,v):
         self.graph[v]
 
+    def dfs_topological_sort(self):
+        def tsutil(v, deq, visited):
+            visited.add(v)
+            for i in self.graph[v]:
+                if i not in visited:
+                    tsutil(i, deq, visited)
+
+            # append to beginning after we've added all this
+            # nodes children
+            deq.appendleft(v)
+
+        deq, visited = deque(), set()
+        for i in self.graph:
+            if i not in visited:
+                tsutil(i, deq, visited)
+
+        print(deq)
+
     def cyclic(self):
 
-        def visit(vertex, maxPath):
+        def visit(vertex):
             if vertex in visited: return False
-            visited.add(vertex)
-            path.add(vertex)
-            maxPath = max(maxPath, len(path))
-            print(maxPath)
+            visited.add(vertex); path.add(vertex);
             for nbor in self.graph[vertex]:
-                if nbor in path or visit(nbor, maxPath):
+                if nbor in path or visit(nbor):
                     return True
             path.remove(vertex)
+            deq.appendleft(vertex)
             return False
 
 
-        path,visited = set(), set()
-        maxPath = 0
+
+        path,visited, deq = set(), set(), deque()
+        components = defaultdict(int) #generate number of components
         for v in self.graph:
-            print(maxPath)
-            if visit(v, maxPath): return True
+            if visit(v):
+                components[len(visited)] += 1
+                # print(components)
+                return True
+            components[len(visited)] += 1
+
+        print(components)
+        return deq
+
 
 
 g = Graph()
@@ -37,8 +61,10 @@ for i in range(5): g.addVertex(i)
 g.addEdge(1,3)
 g.addEdge(3,5)
 g.addEdge(5,2)
-g.addEdge(2,1)
+g.addEdge(1,2)
+g.addEdge
 
 
 
 print(g.cyclic())
+# print(g.dfs_topological_sort())
